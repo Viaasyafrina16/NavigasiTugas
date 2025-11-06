1267b9c76506f58324ce507af016609ad32d97ac
Viaasyafrina16 <syafrinavia@gmail.com>
2025-11-06 23:10:28 +0700

    membuat formIsian

---
+++ app/src/main/java/com/example/navigasitugas/view/DataDiri.kt
@@ -0,0 +1,29 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.navigasitugas.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.navigasiku.R
+
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    onSubmit: () -> Unit
+) {
+
+
+}
+
+
+
