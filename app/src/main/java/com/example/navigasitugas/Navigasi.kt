f6c541dca18b1c1d513c268477c5a9ae02865689
Viaasyafrina16 <syafrinavia@gmail.com>
2025-11-06 21:10:55 +0700

    Membuat route utama

--- app/src/main/java/com/example/navigasitugas/Navigasi.kt
+++ app/src/main/java/com/example/navigasitugas/Navigasi.kt
@@ -1,7 +1,23 @@
 package com.example.navigasitugas
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi{
     Beranda,
     Formulir,
     Detail
 }
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+
+    modifier: Modifier
+) {
